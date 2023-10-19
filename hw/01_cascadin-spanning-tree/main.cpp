6dfbe6357a6cc3fb09636b58ee1f721724ab2cb0
petrkucerak <petr@khome.cz>
2023-10-19 23:11:53 +0200

    Implement priority queue

--- hw/01_cascadin-spanning-tree/main.cpp
+++ hw/01_cascadin-spanning-tree/main.cpp
@@ -1,13 +1,39 @@
-#include <iostream>
+#include <queue>
+#include <stdio.h>
 using namespace std;
+
+struct Edge {
+   int target;
+   int value;
+   Edge(int target, int value) : target(target), value(value) {}
+};
+struct CompareEdge {
+   bool operator()(Edge const &e1, Edge const &e2)
+   {
+      return e1.value > e2.value;
+   }
+};
 
 int main(int argc, char const *argv[])
 {
-   uint64_t cont = 0;
-   printf("Hello world\n");
-   for (uint64_t i = 0; i < 100000; ++i) {
-      cont += i;
+   // load data
+   int number_vertices, number_edges;
+   if (scanf("%d %d\n", &number_vertices, &number_edges) != 2)
+      fprintf(stderr, "Can not load data\n");
+
+   printf("Loaded data %d %d\n", number_vertices, number_edges);
+
+   priority_queue<Edge, vector<Edge>, CompareEdge> q;
+   for (int i = 0; i < 20; ++i) {
+      q.push(Edge(0, 20 - i));
    }
-   printf("%ld\n", cont);
+   for (int i = 0; i < 20; ++i) {
+      Edge tmp = q.top();
+      q.pop();
+      printf("%d ", tmp.value);
+   }
+   printf("\n");
+
+   // calcucle minimal cascading
    return 0;
 }
