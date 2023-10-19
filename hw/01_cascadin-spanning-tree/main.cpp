895588e79cb825450a17709f3fefb37c17cc63c0
petrkucerak <petr@khome.cz>
2023-10-19 23:28:09 +0200

    Load data

--- hw/01_cascadin-spanning-tree/main.cpp
+++ hw/01_cascadin-spanning-tree/main.cpp
@@ -16,24 +16,28 @@ struct CompareEdge {
 
 int main(int argc, char const *argv[])
 {
-   // load data
+   // LOAD DATA
    int number_vertices, number_edges;
    if (scanf("%d %d\n", &number_vertices, &number_edges) != 2)
       fprintf(stderr, "Can not load data\n");
 
-   printf("Loaded data %d %d\n", number_vertices, number_edges);
+   // create array for pririty queues
+   priority_queue<Edge, vector<Edge>, CompareEdge> *data =
+       new priority_queue<Edge, vector<Edge>, CompareEdge>[number_vertices];
 
-   priority_queue<Edge, vector<Edge>, CompareEdge> q;
-   for (int i = 0; i < 20; ++i) {
-      q.push(Edge(0, 20 - i));
+   // load edges data
+   for (int i = 0; i < number_edges; ++i) {
+      int vertex, target, value;
+      if (scanf("%d %d %d\n", &vertex, &target, &value) != 3)
+         fprintf(stderr, "Can not load data\n");
+      data[vertex - 1].push(Edge(target - 1, value));
+      // save symtricity
+      data[target - 1].push(Edge(vertex - 1, value - 1));
    }
-   for (int i = 0; i < 20; ++i) {
-      Edge tmp = q.top();
-      q.pop();
-      printf("%d ", tmp.value);
-   }
-   printf("\n");
 
    // calcucle minimal cascading
+
+   // free space
+   delete[] data;
    return 0;
 }
