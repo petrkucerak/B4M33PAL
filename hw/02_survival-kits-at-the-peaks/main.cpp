c11ba8a80acfd1454cd779e1a4d5b9a98c3e7684
petrkucerak <petr@khome.cz>
2023-10-28 03:20:35 +0200

    Load metadata

--- hw/02_survival-kits-at-the-peaks/main.cpp
+++ hw/02_survival-kits-at-the-peaks/main.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
+#include <stdlib.h>
 
 int main(int argc, char const *argv[])
 {
-   std::cout << "Hello next challange!" << std::endl;
+   // Load metadata
+   int points_count, tracks_count, cp_no;
+   if (scanf("%d %d %d\n", &points_count, &tracks_count, &cp_no) != 3) {
+      fprintf(stderr, "ERROR: Can not load correctly the meta data!\n");
+      exit(EXIT_FAILURE);
+   }
+
+   printf("Points count: %d | Tracks count: %d | CP no: %d\n", points_count,
+          tracks_count, cp_no);
    return 0;
 }
