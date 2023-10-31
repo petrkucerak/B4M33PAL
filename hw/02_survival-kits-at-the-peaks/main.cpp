9395282354d0c7a66454924c393c0cf49b0aebe4
petrkucerak <petr@khome.cz>
2023-10-31 21:13:27 +0100

    Debuging code

--- hw/02_survival-kits-at-the-peaks/main.cpp
+++ hw/02_survival-kits-at-the-peaks/main.cpp
@@ -1,8 +1,49 @@
 #include <iostream>
+#include <stack>
 #include <stdlib.h>
 #include <vector>
 
-#define PRINT_LOADED_DATA
+// #define PRINT_LOADED_DATA
+#define PRINT_LOWLINK
+
+void find_scc(int i, std::stack<int> &stack, std::vector<int> &lowlink,
+              std::vector<int> &indexes, std::vector<std::vector<int>> &data,
+              int &areas, int &package, int &index)
+{
+   indexes[i] = lowlink[i] = index;
+   ++index;
+   stack.push(i);
+   // std::cout << indexes[i] << std::endl;
+   for (long unsigned int n = 0; n < data[i].size(); ++n) {
+      int target = data[i][n];
+      //    std::cout << target << std::endl;
+      if (indexes[target] == 0) { // not yet visited
+         find_scc(target, stack, lowlink, indexes, data, areas, package, index);
+         lowlink[i] = std::min(lowlink[i], lowlink[target]);
+      } else {
+         lowlink[i] = std::min(lowlink[i], indexes[target]);
+      }
+   }
+
+   if (lowlink[i] == indexes[i]) { // point is head of SCC (area)
+      ++areas;
+      std::cout << "Area: " << lowlink[i] << std::endl;
+      int j;
+      do {
+         j = stack.top();
+         stack.pop();
+         ++package;
+         // add j node (point) to the strongly connected component
+      } while (i == j); // output the current strongly connected component
+   }
+#ifdef PRINT_LOWLINK
+   if (i == 0) {
+      for (long unsigned int l = 0; l < lowlink.size(); ++l)
+         printf("%3d ", lowlink.at(l));
+      printf("\n");
+   }
+#endif // end of PRINT_LOWLINK
+}
 
 int main(int argc, char const *argv[])
 {
@@ -34,7 +75,25 @@ int main(int argc, char const *argv[])
       }
       printf("\n");
    }
-#endif // PRINT_LOADED_DATA
+#endif // end of PRINT_LOADED_DATA
+
+   // TODO: start at CP?
+   // implement Tarjan's Algorithm
+
+   // define variables
+   std::stack<int> stack;
+   std::vector<int> lowlink(points_count);
+   std::vector<int> indexes(points_count, 0);
+   int index = 0;
+   int areas = 0;   // Count of SCC
+   int package = 0; // Count of points in the SCC
+
+   for (auto i = 0; i < points_count; ++i) {
+      if (indexes[i] == 0) // is not visited
+         find_scc(i, stack, lowlink, indexes, data, areas, package, index);
+   }
+
+   printf("Areas: %d Packages: %d\n", areas, package);
 
    return 0;
 }
