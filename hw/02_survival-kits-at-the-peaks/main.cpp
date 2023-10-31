df95ccb116c98d8efeb668bbaa0434cdbbb52022
petrkucerak <petr@khome.cz>
2023-10-31 19:03:09 +0100

    Load data

--- hw/02_survival-kits-at-the-peaks/main.cpp
+++ hw/02_survival-kits-at-the-peaks/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <stdlib.h>
+#include <vector>
 
 int main(int argc, char const *argv[])
 {
@@ -12,5 +13,24 @@ int main(int argc, char const *argv[])
 
    printf("Points count: %d | Tracks count: %d | CP no: %d\n", points_count,
           tracks_count, cp_no);
+
+   std::vector<std::vector<int>> data(points_count);
+   for (auto i = 0; i < tracks_count; ++i) {
+      int node, target;
+      if (scanf("%d %d\n", &node, &target) != 2) {
+         fprintf(stderr, "ERROR: Can not load correctly the graph data!\n");
+         exit(EXIT_FAILURE);
+      };
+      data[node].push_back(target); // TODO: make it effective
+   }
+
+   for (auto i = 0; i < points_count; ++i) {
+      printf("%4d | ", i);
+      for (auto j = 0; j < data[i].size(); ++j) {
+         printf("%d ", data[i][j]);
+      }
+      printf("\n");
+   }
+
    return 0;
 }
