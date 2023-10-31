261b6e6140b919c3925784f138ae1b37546a52f6
petrkucerak <petr@khome.cz>
2023-11-01 00:21:00 +0100

    Find condesed componends

--- hw/02_survival-kits-at-the-peaks/main.cpp
+++ hw/02_survival-kits-at-the-peaks/main.cpp
@@ -4,44 +4,68 @@
 #include <vector>
 
 // #define PRINT_LOADED_DATA
-#define PRINT_LOWLINK
+// #define PRINT_LOWLINK
+
+struct Area {
+   std::vector<int> *nodes;
+   // int tracks_cout; can use nodes.size()
+
+   Area(std::vector<int> *nodes /*, int tracks_cout*/)
+       : nodes(nodes) /*,tracks_cout(tracks_cout)*/
+   {
+   }
+};
 
 void find_scc(int i, std::stack<int> &stack, std::vector<int> &lowlink,
-              std::vector<int> &indexes, std::vector<std::vector<int>> &data,
-              int &areas, int &package, int &index)
+              std::vector<int> &indexes, std::vector<bool> &in_stack,
+              std::vector<std::vector<int>> &data, std::vector<Area> &area,
+              int &index, int &cp_area, int &cp_no)
 {
-   indexes[i] = lowlink[i] = index;
-   ++index;
+   indexes[i] = lowlink[i] = ++index;
    stack.push(i);
-   // std::cout << indexes[i] << std::endl;
+   in_stack[i] = true;
    for (long unsigned int n = 0; n < data[i].size(); ++n) {
       int target = data[i][n];
-      //    std::cout << target << std::endl;
       if (indexes[target] == 0) { // not yet visited
-         find_scc(target, stack, lowlink, indexes, data, areas, package, index);
+         find_scc(target, stack, lowlink, indexes, in_stack, data, area, index,
+                  cp_area, cp_no);
          lowlink[i] = std::min(lowlink[i], lowlink[target]);
-      } else {
+      } else if (in_stack[target]) {
          lowlink[i] = std::min(lowlink[i], indexes[target]);
       }
    }
 
    if (lowlink[i] == indexes[i]) { // point is head of SCC (area)
-      ++areas;
-      std::cout << "Area: " << lowlink[i] << std::endl;
+      std::vector<int> *t = new std::vector<int>;
+      area.push_back(Area(t));
+      int k = area.size() - 1;
+
       int j;
-      do {
+      while (stack.top() != i) {
          j = stack.top();
+         in_stack[j] = false;
          stack.pop();
-         ++package;
-         // add j node (point) to the strongly connected component
-      } while (i == j); // output the current strongly connected component
+
+         // Add point to area
+         // ++area[k].tracks_cout;
+         area[k].nodes->push_back(j);
+         if (j == cp_no)
+            cp_area = k;
+      }
+      j = stack.top();
+      in_stack[j] = false;
+      stack.pop();
+
+      // Add point to area
+      // ++area[k].tracks_cout;
+      area[k].nodes->push_back(j);
+      if (j == cp_no)
+         cp_area = k;
    }
 #ifdef PRINT_LOWLINK
-   if (i == 0) {
-      for (long unsigned int l = 0; l < lowlink.size(); ++l)
-         printf("%3d ", lowlink.at(l));
-      printf("\n");
-   }
+   for (long unsigned int l = 0; l < lowlink.size(); ++l)
+      printf("%3d ", lowlink.at(l));
+   printf("\n");
 #endif // end of PRINT_LOWLINK
 }
 
@@ -82,18 +106,26 @@ int main(int argc, char const *argv[])
 
    // define variables
    std::stack<int> stack;
+   std::vector<bool> in_stack(points_count, false);
    std::vector<int> lowlink(points_count);
    std::vector<int> indexes(points_count, 0);
+   std::vector<Area> area;
+   int cp_area;
    int index = 0;
-   int areas = 0;   // Count of SCC
-   int package = 0; // Count of points in the SCC
 
    for (auto i = 0; i < points_count; ++i) {
       if (indexes[i] == 0) // is not visited
-         find_scc(i, stack, lowlink, indexes, data, areas, package, index);
+      {
+         find_scc(i, stack, lowlink, indexes, in_stack, data, area, index,
+                  cp_area, cp_no);
+         // std::cout << "Start from node: " << i << std::endl;
+      }
    }
 
-   printf("Areas: %d Packages: %d\n", areas, package);
+   // remove struct
+   for (long unsigned int i = 0; i < area.size(); ++i) {
+      delete area[i].nodes;
+   }
 
    return 0;
 }
