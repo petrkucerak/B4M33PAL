0b45dfa2ca492a554477f68d858f1f4697820b17
petrkucerak <petr@khome.cz>
2023-11-15 00:38:36 +0100

    Throw all servers

--- hw/03_network-topology-fix/main.cpp
+++ hw/03_network-topology-fix/main.cpp
@@ -1,26 +1,33 @@
 #include <iostream>
 #include <vector>
 
+#define MAP(x, y) ((x) + (servers_num * (y)))
+
 using namespace std;
 
-struct Server {
-   bool is_fast = false;
-   vector<int> to;
+struct Point {
+   int x;
+   int y;
 };
 
-void print_network(vector<Server> S)
+struct Result {
+   int a, b; // a, b (a < b) are the labels of two servers in network A
+   int c, d; // c, d (c < d) are the labels of two servers in network B
+};
+
+void print_map(vector<int> map, int servers_num)
 {
    cout << endl;
-   for (auto &e : S) {
-      cout << e.is_fast << " | ";
-      for (auto &c : e.to)
-         cout << c << " ";
+   for (int y = 0; y < servers_num; ++y) {
+      for (int x = 0; x < servers_num; ++x)
+         cout << map[MAP(x, y)] << " ";
       cout << endl;
    }
 }
 
 int main(int argc, char const *argv[])
 {
+   // LOAD THE DATA
    int servers_num, planned_connection_num, fast_servers_num;
    if (scanf("%d %d %d\n", &servers_num, &planned_connection_num,
              &fast_servers_num) != 3) {
@@ -28,7 +35,10 @@ int main(int argc, char const *argv[])
       exit(EXIT_FAILURE);
    }
 
-   vector<Server> A(servers_num);
+   const int map_size = servers_num * servers_num;
+
+   vector<bool> A_map_fast(servers_num, false);
+   vector<int> A_map(map_size, 0);
    // Load fast servers for A network
    for (int i = 0; i < fast_servers_num; ++i) {
       int tmp;
@@ -36,7 +46,7 @@ int main(int argc, char const *argv[])
          fprintf(stderr, "Can't load list of fast servers for A network\n");
          exit(EXIT_FAILURE);
       }
-      A[tmp].is_fast = true;
+      A_map_fast[tmp] = true;
    }
    // Load fast connection for A network
    for (int i = 0; i < planned_connection_num - 1; ++i) {
@@ -45,11 +55,13 @@ int main(int argc, char const *argv[])
          fprintf(stderr, "Can't load connection for A network\n");
          exit(EXIT_FAILURE);
       }
-      A[a].to.push_back(b);
-      A[b].to.push_back(a);
+      A_map[MAP(b, a)] = 1;
+      A_map[MAP(a, b)] = 1;
    }
 
-   vector<Server> B(servers_num);
+   vector<bool> B_map_fast(servers_num, false);
+   vector<int> B_map(map_size, 0);
+   vector<Point> different;
    // Load fast servers for B network
    for (int i = 0; i < fast_servers_num; ++i) {
       int tmp;
@@ -57,7 +69,7 @@ int main(int argc, char const *argv[])
          fprintf(stderr, "Can't load list of fast servers for B network\n");
          exit(EXIT_FAILURE);
       }
-      B[tmp].is_fast = true;
+      B_map_fast[tmp] = true;
    }
    // Load fast connection for B network
    for (int i = 0; i < planned_connection_num + 1; ++i) {
@@ -66,12 +78,53 @@ int main(int argc, char const *argv[])
          fprintf(stderr, "Can't load connection for B network\n");
          exit(EXIT_FAILURE);
       }
-      B[a].to.push_back(b);
-      B[b].to.push_back(a);
+      B_map[MAP(b, a)] = 1;
+      B_map[MAP(a, b)] = 1;
+      if (!A_map[MAP(b, a)])
+         different.push_back({b, a});
    }
 
-   print_network(A);
-   print_network(B);
+   // print_network(A);
+   // print_network(B);
 
+   print_map(A_map, servers_num);
+   print_map(B_map, servers_num);
+
+   vector<Result> results;
+
+   // HERUISTIC
+   // 1. pair only server with same num of connection
+   // 2. pair only fast server to fast server
+
+   // START CALCULATION
+   // 1. Add and remove connection to every possible connection
+   // 2. Sort by connection count
+   // 3. verify isomorfism
+
+   // Print differents
+   // for (auto &d : different)
+   //    cout << "[" << d.x << "," << d.y << "] ";
+   // cout << endl;
+
+   for (auto &point : different) {
+      // remove connection
+      B_map[MAP(point.x, point.y)] = 0;
+      B_map[MAP(point.y, point.x)] = 0;
+      for (int y = 0; y < servers_num - 1; ++y) {
+         for (int x = y + 1; x < servers_num; ++x) {
+            if (A_map[MAP(x, y)]) // connection exists in A network
+               continue;
+            A_map[MAP(x, y)] = 1;
+            // check isomorfism
+         }
+      }
+      // add connection back
+      B_map[MAP(point.x, point.y)] = 1;
+      B_map[MAP(point.y, point.x)] = 1;
+   }
+
+   // PRINT RESULTS
+   for (auto result : results)
+      printf("%d %d %d %d\n", result.a, result.b, result.c, result.d);
    return 0;
 }
