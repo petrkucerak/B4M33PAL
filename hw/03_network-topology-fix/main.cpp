8b20691bbde292092b487f824451a68061f6d347
petrkucerak <petr@khome.cz>
2023-11-16 00:28:20 +0100

    Load data

--- hw/03_network-topology-fix/main.cpp
+++ hw/03_network-topology-fix/main.cpp
@@ -1,51 +1,24 @@
+#include <algorithm>
 #include <iostream>
+#include <unordered_set>
 #include <vector>
-
-#define MAP(x, y) ((x) + (servers_num * (y)))
 
 using namespace std;
 
-struct Point {
-   int x;
-   int y;
+struct Server {
+   unordered_set<int> target;
+   bool is_fast = false;
 };
 
-struct Result {
-   int a, b; // a, b (a < b) are the labels of two servers in network A
-   int c, d; // c, d (c < d) are the labels of two servers in network B
-};
-
-void print_map(vector<int> map, int servers_num)
+void print_network(vector<Server> servers)
 {
    cout << endl;
-   for (int y = 0; y < servers_num; ++y) {
-      for (int x = 0; x < servers_num; ++x)
-         cout << map[MAP(x, y)] << " ";
+   for (auto &server : servers) {
+      cout << server.is_fast << " | ";
+      for (auto &target : server.target)
+         cout << target << " ";
       cout << endl;
    }
-}
-
-bool is_isomorfism(vector<int> A_map, vector<int> B_map,
-                   vector<bool> A_map_fast, vector<bool> B_map_fast,
-                   int servers_num)
-{
-   // HERUISTIC
-   // 1. pair only server with same num of connection
-   vector<vector<int>> A_connection(servers_num - 1);
-   vector<vector<int>> B_connection(servers_num - 1);
-   for (int i = 0; i < servers_num; ++i) {
-      // for each row, get sum of connection
-      int A_sum = 0;
-      int B_sum = 0;
-      for (int j = 0; j < servers_num; ++j) {
-         A_sum += A_map[MAP(j, i)];
-         B_sum += B_map[MAP(j, i)];
-      }
-      // A_connection[A_sum].push_back(i);
-      cout << A_sum << endl;
-      B_connection[B_sum].push_back(i);
-   }
-   return false;
 }
 
 int main(int argc, char const *argv[])
@@ -57,11 +30,7 @@ int main(int argc, char const *argv[])
       fprintf(stderr, "Can't load the metadata\n");
       exit(EXIT_FAILURE);
    }
-
-   const int map_size = servers_num * servers_num;
-
-   vector<bool> A_map_fast(servers_num, false);
-   vector<int> A_map(map_size, 0);
+   vector<Server> A(servers_num);
    // Load fast servers for A network
    for (int i = 0; i < fast_servers_num; ++i) {
       int tmp;
@@ -69,96 +38,41 @@ int main(int argc, char const *argv[])
          fprintf(stderr, "Can't load list of fast servers for A network\n");
          exit(EXIT_FAILURE);
       }
-      A_map_fast[tmp] = true;
+      A[tmp].is_fast = true;
    }
-   // Load fast connection for A network
+   // Load connection for A network
    for (int i = 0; i < planned_connection_num - 1; ++i) {
       int a, b;
       if (scanf("%d %d\n", &a, &b) != 2) {
          fprintf(stderr, "Can't load connection for A network\n");
          exit(EXIT_FAILURE);
       }
-      A_map[MAP(b, a)] = 1;
-      A_map[MAP(a, b)] = 1;
+      A[a].target.insert(b);
+      A[b].target.insert(a);
    }
-
-   vector<bool> B_map_fast(servers_num, false);
-   vector<int> B_map(map_size, 0);
-   vector<Point> different;
+   vector<Server> B(servers_num);
    // Load fast servers for B network
    for (int i = 0; i < fast_servers_num; ++i) {
       int tmp;
       if (scanf("%d", &tmp) != 1) {
-         fprintf(stderr, "Can't load list of fast servers for B network\n");
+         fprintf(stderr, "Can't load list of fast servers for A network\n");
          exit(EXIT_FAILURE);
       }
-      B_map_fast[tmp] = true;
+      B[tmp].is_fast = true;
    }
-   // Load fast connection for B network
+   // Load connection for B network
    for (int i = 0; i < planned_connection_num + 1; ++i) {
       int a, b;
       if (scanf("%d %d\n", &a, &b) != 2) {
-         fprintf(stderr, "Can't load connection for B network\n");
+         fprintf(stderr, "Can't load connection for A network\n");
          exit(EXIT_FAILURE);
       }
-      B_map[MAP(b, a)] = 1;
-      B_map[MAP(a, b)] = 1;
-      if (!A_map[MAP(b, a)])
-         different.push_back({b, a});
+      B[a].target.insert(b);
+      B[b].target.insert(a);
    }
 
    // print_network(A);
    // print_network(B);
 
-   print_map(A_map, servers_num);
-   print_map(B_map, servers_num);
-
-   vector<Result> results;
-
-   // HERUISTIC
-   // 1. pair only server with same num of connection
-   // 2. pair only fast server to fast server
-
-   // START CALCULATION
-   // 1. Add and remove connection to every possible connection
-   // 2. Sort by connection count
-   // 3. verify isomorfism
-
-   // Print differents
-   // for (auto &d : different)
-   //    cout << "[" << d.x << "," << d.y << "] ";
-   // cout << endl;
-
-   for (auto &point : different) {
-      // remove connection
-      B_map[MAP(point.x, point.y)] = 0;
-      B_map[MAP(point.y, point.x)] = 0;
-      for (int y = 0; y < servers_num - 1; ++y) {
-         for (int x = y + 1; x < servers_num; ++x) {
-            if (A_map[MAP(x, y)]) // connection exists in A network
-               continue;
-            // add connection
-            A_map[MAP(x, y)] = 1;
-            A_map[MAP(y, x)] = 1;
-
-            if (is_isomorfism(A_map, B_map, A_map_fast, B_map_fast,
-                              servers_num)) {
-               int a, b, c, d;
-               point.x < point.y ? c = point.x, d = point.y : c = point.y,
-                                   d = point.x;
-               x < y ? a = x, b = y : a = y, b = x;
-               printf("%d %d %d %d\n", a, b, c, d);
-            }
-
-            // add connection back
-            B_map[MAP(point.x, point.y)] = 1;
-            B_map[MAP(point.y, point.x)] = 1;
-         }
-      }
-   }
-
-   // PRINT RESULTS
-   for (auto result : results)
-      printf("%d %d %d %d\n", result.a, result.b, result.c, result.d);
    return 0;
 }
