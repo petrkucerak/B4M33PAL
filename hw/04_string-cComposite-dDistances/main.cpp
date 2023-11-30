a9a359bf15384d49b18a9bf1488ed9d16cd37f2f
petrkucerak <petr@khome.cz>
2023-11-30 15:28:08 +0100

    Load the data

--- hw/04_string-cComposite-dDistances/main.cpp
+++ hw/04_string-cComposite-dDistances/main.cpp
@@ -2,6 +2,44 @@
 
 int main(int argc, char const *argv[])
 {
-   std::cout << "Hello world" << std::endl;
+   int s_length, t_length, length_min, length_max;
+   if (scanf("%d %d %d %d\n", &s_length, &t_length, &length_min, &length_max) !=
+       4) {
+      fprintf(stderr, "Can't load metadata!\n");
+      exit(EXIT_FAILURE);
+   }
+   int *S = new int[s_length];
+   int *T = new int[t_length];
+
+   for (int i = 0; i < s_length; ++i) {
+      char n;
+      if (scanf("%c", &n) != 1) {
+         fprintf(stderr, "Can't load S string!\n");
+         exit(EXIT_FAILURE);
+      }
+      S[i] = int(n) - 48;
+   }
+   scanf("\n");
+   for (int i = 0; i < t_length; ++i) {
+      char n;
+      if (scanf("%c", &n) != 1) {
+         fprintf(stderr, "Can't load T string!\n");
+         exit(EXIT_FAILURE);
+      }
+      T[i] = int(n) - 48;
+   }
+
+   // Print S array
+   for (int i = 0; i < s_length; ++i)
+      printf("%d", S[i]);
+   printf("\n");
+   // Print T array
+   for (int i = 0; i < t_length; ++i)
+      printf("%d", T[i]);
+   printf("\n");
+
+   delete[] S;
+   delete[] T;
+
    return 0;
 }
