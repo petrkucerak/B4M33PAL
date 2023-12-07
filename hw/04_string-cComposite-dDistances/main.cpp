caeba702cce0f5a29912e47fea89f950efa99963
petrkucerak <petr@khome.cz>
2023-12-07 15:26:50 +0100

    Get trie metadata

--- hw/04_string-cComposite-dDistances/main.cpp
+++ hw/04_string-cComposite-dDistances/main.cpp
@@ -15,16 +15,22 @@ struct Node {
    Node() : type(0), add{NULL, NULL}, depth(0) {}
 };
 
-void build_trie(Node *root, string pattern, string path, const int max_depth)
+int global_depth = 0;
+int leaf_count = 0;
+
+void build_trie(Node *root, string pattern, string path, const int max_depth,
+                const int min_depth)
 {
-   if (root->depth <= max_depth) {
+   if (root->depth < max_depth) {
+      bool is_leaf = true;
       string path_0 = path;
       path_0.append(sizeof(char), '0');
       if (pattern.find(path_0) != string::npos) {
          root->add[0] = new Node;
          root->add[0]->type = TYPE_0;
          root->add[0]->depth = root->depth + 1;
-         build_trie(root->add[0], pattern, path_0, max_depth);
+         build_trie(root->add[0], pattern, path_0, max_depth, min_depth);
+         is_leaf = false;
       }
 
       string path_1 = path;
@@ -33,11 +39,21 @@ void build_trie(Node *root, string pattern, string path, const int max_depth)
          root->add[1] = new Node;
          root->add[1]->type = TYPE_1;
          root->add[1]->depth = root->depth + 1;
-         build_trie(root->add[1], pattern, path_1, max_depth);
+         build_trie(root->add[1], pattern, path_1, max_depth, min_depth);
+         is_leaf = false;
+      }
+      if (is_leaf) {
+         // cout << path << endl;
+         if (path.size() >= min_depth) {
+            ++leaf_count;
+            global_depth += root->depth;
+         }
       }
    } else {
       // Only for testing
-      cout << path << endl;
+      // cout << path << endl;
+      ++leaf_count;
+      global_depth += root->depth;
    }
 }
 
@@ -89,13 +105,13 @@ int main(int argc, char const *argv[])
    }
 
    // Print S array
-   for (int i = 0; i < s_length; ++i)
-      printf("%d", S[i]);
-   printf("\n");
-   // Print T array
-   for (int i = 0; i < t_length; ++i)
-      printf("%d", T[i]);
-   printf("\n");
+   // for (int i = 0; i < s_length; ++i)
+   //    printf("%d", S[i]);
+   // printf("\n");
+   // // Print T array
+   // for (int i = 0; i < t_length; ++i)
+   //    printf("%d", T[i]);
+   // printf("\n\n");
 
    Node *root = new Node;
    root->depth = 0;
@@ -104,15 +120,18 @@ int main(int argc, char const *argv[])
       root->add[0] = new Node;
       root->add[0]->type = TYPE_0;
       root->add[0]->depth = root->depth + 1;
-      build_trie(root->add[0], pattern, "0", length_max);
+      build_trie(root->add[0], pattern, "0", length_max, length_min);
    }
 
    if (pattern.find("1") != string::npos) {
       root->add[1] = new Node;
       root->add[1]->type = TYPE_1;
       root->add[1]->depth = root->depth + 1;
-      build_trie(root->add[1], pattern, "1", length_max);
+      build_trie(root->add[1], pattern, "1", length_max, length_min);
    }
+
+   cout << "Leaf count: " << leaf_count << endl;
+   cout << "Global depth: " << global_depth << endl;
 
    delete_trie(&root);
 
