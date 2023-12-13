a75421153077be24f9e14e8cc70185b5d50c1759
petrkucerak <petr@khome.cz>
2023-12-13 15:16:20 +0100

    Improve with chatGPT

--- hw/04_string-cComposite-dDistances/main.cpp
+++ hw/04_string-cComposite-dDistances/main.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
 #include <vector>
 
-#define CHILD root->to[pattern[pattern_start]]
-
 struct Node {
    bool type;
    Node *to[2];
@@ -10,127 +8,131 @@ struct Node {
    int occurrence;
    bool leaf;
 
-   Node() : type(0), to{NULL, NULL}, depth(0), occurrence(0), leaf(false) {}
+   Node()
+       : type(false), to{nullptr, nullptr}, depth(0), occurrence(0), leaf(false)
+   {
+   }
 };
 
-using namespace std;
-
-int global_depth = 0;
-int leaf_count = 0;
-
-void delete_trie(Node **root)
+// Function to delete the trie
+void deleteTrie(Node *&root)
 {
-   if (*root == NULL)
+   if (root == nullptr) {
       return;
-   if ((*root)->to[0] != NULL) {
-      delete_trie(&(*root)->to[0]);
-      (*root)->to[0] = NULL;
    }
-   if ((*root)->to[1] != NULL) {
-      delete_trie(&(*root)->to[1]);
-      (*root)->to[1] = NULL;
+   if (root->to[0] != nullptr) {
+      deleteTrie(root->to[0]);
+      root->to[0] = nullptr;
    }
-   delete *root;
-   *root = NULL;
+   if (root->to[1] != nullptr) {
+      deleteTrie(root->to[1]);
+      root->to[1] = nullptr;
+   }
+   delete root;
+   root = nullptr;
 }
 
-Node *find_node(Node *root, vector<bool> pattern, int pattern_start,
-                int pattern_end)
+// Function to find a node in the trie
+Node *findNode(Node *root, const std::vector<bool> &pattern, int pattern_start,
+               int pattern_end)
 {
    Node *tmp = root;
    for (int i = pattern_start; i < pattern_end; ++i) {
-      if (tmp->to[pattern[i]] != NULL)
+      if (tmp->to[pattern[i]] != nullptr)
          tmp = tmp->to[pattern[i]];
       else
-         return NULL;
+         return nullptr;
    }
    return tmp;
 }
 
-void add_pattern(Node *root, vector<bool> pattern, int pattern_start,
-                 int pattern_end, const int max_depth, const int min_depth)
+// Function to add a pattern to the trie
+void addPattern(Node *root, const std::vector<bool> &pattern, int pattern_start,
+                int pattern_end, const int max_depth, const int min_depth,
+                int &leaf_count, int &global_depth)
 {
-   if (CHILD == NULL) {
-      CHILD = new Node;
-      CHILD->depth = root->depth + 1;
-      CHILD->type = pattern[pattern_start];
+   Node *&child = root->to[pattern[pattern_start]];
+   if (child == nullptr) {
+      child = new Node;
+      child->depth = root->depth + 1;
+      child->type = pattern[pattern_start];
    }
    if (pattern_end > pattern_start + 1)
-      add_pattern(CHILD, pattern, pattern_start + 1, pattern_end, max_depth,
-                  min_depth);
+      addPattern(child, pattern, pattern_start + 1, pattern_end, max_depth,
+                 min_depth, leaf_count, global_depth);
    else {
       // final stage
-      if (CHILD->depth >= min_depth)
-         CHILD->occurrence += 1;
+      if (child->depth >= min_depth)
+         child->occurrence += 1;
       // is leaf?
-      if (!CHILD->leaf && CHILD->to[0] == NULL && CHILD->to[1] == NULL) {
-         CHILD->leaf = true;
+      if (!child->leaf && child->to[0] == nullptr && child->to[1] == nullptr) {
+         child->leaf = true;
          ++leaf_count;
-         global_depth += CHILD->depth;
+         global_depth += child->depth;
       }
    }
 }
 
-int main(int argc, char const *argv[])
+// Function to compute the RDC
+long int computeRDC(Node *root, const std::vector<bool> &t_pattern,
+                    int length_min, int length_max)
 {
-   // LOAD DATA
-   int s_length, t_length, length_min, length_max;
-   if (scanf("%d %d %d %d\n", &s_length, &t_length, &length_min, &length_max) !=
-       4) {
-      fprintf(stderr, "Can't load metadata!\n");
-      exit(EXIT_FAILURE);
-   }
-
-   vector<bool> s_pattern;
-   vector<bool> t_pattern;
-   s_pattern.reserve(s_length);
-   t_pattern.reserve(t_length);
-
-   for (int i = 0; i < s_length; ++i) {
-      char n;
-      if (scanf("%c", &n) != 1) {
-         fprintf(stderr, "Can't load S string!\n");
-         exit(EXIT_FAILURE);
-      }
-      s_pattern.push_back(n == '0' ? false : true);
-   }
-   scanf("\n");
-   for (int i = 0; i < t_length; ++i) {
-      char n;
-      if (scanf("%c", &n) != 1) {
-         fprintf(stderr, "Can't load T string!\n");
-         exit(EXIT_FAILURE);
-      }
-      t_pattern.push_back(n == '0' ? false : true);
-   }
-
-   // COMPUTE TRIE
-   Node *root = new Node;
-   root->depth = 0;
-   for (int pattern_length = length_max; pattern_length >= length_min;
-        --pattern_length) {
-      for (int pattern_start = 0; pattern_start < s_length + 1 - pattern_length;
-           ++pattern_start) {
-         add_pattern(root, s_pattern, pattern_start,
-                     pattern_start + pattern_length, length_max, length_min);
-      }
-   }
-
-   // Calcule RCD
    long int RDC = 0;
-   for (int lenght = length_min; lenght <= length_max; ++lenght) {
-      for (int id = 0; id <= t_length - lenght; ++id) {
-         Node *tmp = find_node(root, t_pattern, id, id + lenght);
-         if (tmp != NULL) {
+   for (int length = length_min; length <= length_max; ++length) {
+      for (int id = 0; id <= t_pattern.size() - length; ++id) {
+         Node *tmp = findNode(root, t_pattern, id, id + length);
+         if (tmp != nullptr) {
             RDC += (tmp->depth * tmp->occurrence);
          }
       }
    }
+   return RDC;
+}
 
-   // REMOVE DATA
-   delete_trie(&root);
+int main()
+{
+   // Load data
+   int s_length, t_length, length_min, length_max;
+   std::cin >> s_length >> t_length >> length_min >> length_max;
 
-   printf("%ld %d %d\n", RDC, leaf_count, global_depth);
+   std::vector<bool> s_pattern(s_length);
+   std::vector<bool> t_pattern(t_length);
+
+   for (int i = 0; i < s_length; ++i) {
+      char n;
+      std::cin >> n;
+      s_pattern[i] = (n == '0' ? false : true);
+   }
+
+   for (int i = 0; i < t_length; ++i) {
+      char n;
+      std::cin >> n;
+      t_pattern[i] = (n == '0' ? false : true);
+   }
+
+   // Compute trie
+   Node *root = new Node;
+   root->depth = 0;
+   int leaf_count = 0;
+   int global_depth = 0;
+   for (int pattern_length = length_max; pattern_length >= length_min;
+        --pattern_length) {
+      for (int pattern_start = 0; pattern_start < s_length + 1 - pattern_length;
+           ++pattern_start) {
+         addPattern(root, s_pattern, pattern_start,
+                    pattern_start + pattern_length, length_max, length_min,
+                    leaf_count, global_depth);
+      }
+   }
+
+   // Compute RDC
+   long int RDC = computeRDC(root, t_pattern, length_min, length_max);
+
+   // Remove data
+   deleteTrie(root);
+
+   // Output result
+   std::cout << RDC << " " << leaf_count << " " << global_depth << std::endl;
 
    return 0;
 }
