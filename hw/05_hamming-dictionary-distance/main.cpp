716bdca166784ae31b57e4dccc0f2fa5c195c109
petrkucerak <petr@khome.cz>
2023-12-25 22:40:51 +0100

    Load the input

--- hw/05_hamming-dictionary-distance/main.cpp
+++ hw/05_hamming-dictionary-distance/main.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
+#include <string>
+#include <vector>
 
 using namespace std;
 
 int main(int argc, char const *argv[])
 {
-   cout << "Hello wordl" << endl;
+   // Load the input
+   int dictionary_size, minumum_dictionary_word_size;
+   cin >> dictionary_size >> minumum_dictionary_word_size;
+   vector<string> dictionary(dictionary_size);
+   for (int i = 0; i < dictionary_size; ++i) {
+      cin >> dictionary[i];
+   }
+   string text;
+   cin >> text;
+
    return 0;
 }
