39a28dd1cd936391532f554f0c6daa83f545263d
petrkucerak <petr@khome.cz>
2024-01-04 04:29:44 +0100

    Load metadata

--- exam/20240105_preterm/main.cpp
+++ exam/20240105_preterm/main.cpp
@@ -6,6 +6,11 @@ using namespace std;
 int main(int argc, char const *argv[])
 {
    // Load the input
-   cout << "Hello preterm exam" << endl;
+   int count_mulecules, count_atoms, count_bonds;
+   if (scanf("%d %d %d\n", &count_mulecules, &count_atoms, &count_bonds) != 3) {
+      fprintf(stderr, "ERROR - can't load the metadata!\n");
+      exit(EXIT_FAILURE);
+   }
+   printf("M: %d, A: %d, B: %d\n", count_mulecules, count_atoms, count_bonds);
    return 0;
 }
