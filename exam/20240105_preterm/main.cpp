21d18e070de9036a285d3bd7ac78bcbc25f0dd65
petrkucerak <petr@khome.cz>
2024-01-04 05:23:03 +0100

    Handle isomorfism checker output

--- exam/20240105_preterm/main.cpp
+++ exam/20240105_preterm/main.cpp
@@ -8,6 +8,8 @@ struct Atom {
    unordered_set<int> target;
 };
 
+bool is_isomorfism(vector<Atom> source, vector<Atom> image) { return false; }
+
 int main(int argc, char const *argv[])
 {
    // Load the metadata
@@ -36,20 +38,35 @@ int main(int argc, char const *argv[])
 
    // Map ordered molecules
    vector<bool> is_ordered(count_mulecules, false);
+   vector<int> group_counts;
 
    for (int i = 0; i < count_mulecules; ++i) {
       // If source molecule is orderd, skip it
       if (is_ordered[i])
          continue;
+
+      // Create new class
       is_ordered[i] = true;
+      group_counts.push_back(1);
+
       for (int j = i + 1; j < count_mulecules; ++j) {
          // If molecule is orderd, skip it
          if (is_ordered[j])
             continue;
 
          // Valid isomorfism
+         if (is_isomorfism(molecules[i], molecules[j])) {
+            is_ordered[j] = true;
+            // incereas last index
+            group_counts.back() += 1;
+         }
       }
    }
+
+   // Print results
+   for (auto group : group_counts)
+      cout << group << " ";
+   cout << endl;
 
    return 0;
 }
