d857f6f93168d3f321f04f42de12e5dab3b13a43
petrkucerak <petr@khome.cz>
2024-01-04 05:02:17 +0100

    Load data

--- exam/20240105_preterm/main.cpp
+++ exam/20240105_preterm/main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
+#include <unordered_set>
 #include <vector>
 
 using namespace std;
+
+struct Atom {
+   unordered_set<int> target;
+};
 
 int main(int argc, char const *argv[])
 {
@@ -12,5 +17,21 @@ int main(int argc, char const *argv[])
       exit(EXIT_FAILURE);
    }
    printf("M: %d, A: %d, B: %d\n", count_mulecules, count_atoms, count_bonds);
+
+   vector<vector<Atom>> molecules(count_mulecules, vector<Atom>(count_atoms));
+   for (auto &molecule : molecules) {
+      for (int i = 0; i < count_bonds; ++i) {
+         int a, b;
+         if (scanf("%d %d\n", &a, &b) != 2) {
+            fprintf(stderr, "ERROR - can't load the data!\n");
+            exit(EXIT_FAILURE);
+         }
+         a -= 1;
+         b -= 1;
+         molecule[a].target.insert(b);
+         molecule[b].target.insert(a);
+      }
+   }
+
    return 0;
 }
