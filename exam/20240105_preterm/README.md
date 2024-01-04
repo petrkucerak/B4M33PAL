cfbbb6865b16ef7e899558f7d6b8ae7b340391ed
petrkucerak <petr@khome.cz>
2024-01-04 04:00:03 +0100

    Create ENV for exam

---
+++ exam/20240105_preterm/README.md
@@ -0,0 +1,2 @@
+# Preterm exam
+
---
+++ exam/20240105_preterm/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+   // Load the input
+   cout << "Hello preterm exam" << endl;
+   return 0;
+}
