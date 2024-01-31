cabbcad7f3b844b5466a50ccd2fe969ae48af06a
petrkucerak <petr@khome.cz>
2024-01-31 09:45:32 +0100

    Load the data

--- exam/20240131_term3/main.cpp
+++ exam/20240131_term3/main.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int main(int argc, char const *argv[])
 {
-   cout << "Hello wordl" << endl;
+   int p_max, m_max, n;
+   if (scanf("%d %d %d\n", &p_max, &m_max, &n) != 3) {
+      fprintf(stderr, "ERROR - can't load the metadata!\n");
+      exit(EXIT_FAILURE);
+   }
+   printf("Metadata: %d %d %d\n", p_max, m_max, n);
+   vector<long> sequence(n);
+   for (auto &x : sequence) {
+      if (scanf("%ld", &x) != 1) {
+         fprintf(stderr, "ERROR - can't load the data!\n");
+         exit(EXIT_FAILURE);
+      }
+   }
+   for (auto &x : sequence)
+      cout << x << " ";
+   cout << endl;
    return 0;
 }
