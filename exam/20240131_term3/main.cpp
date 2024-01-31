de3496d2a1243b72933ac2ca4c582654a4ac5b1d
petrkucerak <petr@khome.cz>
2024-01-31 13:09:03 +0100

    Cut min M

--- exam/20240131_term3/main.cpp
+++ exam/20240131_term3/main.cpp
@@ -11,7 +11,7 @@ const int prime_numbers[] = {
     277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359,
     367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443,
     449, 457, 461, 463, 467, 479, 487, 491, 499, 503};
-const int prime_numbers_count = 94;
+int prime_numbers_count = 94;
 
 long multiplicative_inverse(long a, long modulus)
 {
@@ -31,6 +31,7 @@ long multiplicative_inverse(long a, long modulus)
 void is_valid(vector<long> &sequnece, long A, long C, long M)
 {
    for (int i = 0; i < sequnece.size() - 1; ++i) {
+
       if ((A * sequnece[i] + C) % M != sequnece[i + 1])
          return;
    }
@@ -38,27 +39,45 @@ void is_valid(vector<long> &sequnece, long A, long C, long M)
    exit(EXIT_SUCCESS);
 }
 
+/**
+ * @brief Generate potencial A
+ * A < M
+ * A > min_p
+ * A = (prime_number[0] * primer_number[1] * ... * prime_number[F])
+ */
 void a_combination(int &m_max, long p, vector<long> &sequnece)
 {
    long M = p * p;
    long t_A = p;
    for (int i = 1; (t_A * i) < M; ++i) {
       for (long c = 1; c < M; ++c) {
+         // if (t_A * i + 1 == 70)
+         //    printf("%d %d %d\n", t_A * i + 1, c, M);
          is_valid(sequnece, (t_A * i) + 1, c, M);
       }
    }
 }
 
-void m_combination(int start, long p, int &m_max, vector<long> &sequnece)
+/**
+ * @brief Generate potencial M
+ * M > m_min
+ * M < m_max
+ * M = (prime_numbers[0] * prime_numbers[1] * ... * prime_numbers[F])^2,
+ * where prime_numbers[F]^2 < m_max
+ */
+void m_combination(int start, long p, int &m_max, long &m_min,
+                   vector<long> &sequnece)
 {
    // call next combination
-   a_combination(m_max, p, sequnece);
+   if (m_min <= p * p)
+      a_combination(m_max, p, sequnece);
+   // cout << prime_numbers[start] << endl;
    start += 1;
    for (; start < prime_numbers_count; ++start) {
-      p *= prime_numbers[start];
-      if (p * p > m_max)
+      long tmp_p = p * prime_numbers[start];
+      if (tmp_p * tmp_p > m_max)
          break;
-      m_combination(start, p, m_max, sequnece);
+      m_combination(start, tmp_p, m_max, m_min, sequnece);
    }
 }
 
@@ -85,26 +104,19 @@ int main(int argc, char const *argv[])
    }
 
    // cout << multiplicative_inverse(114, 25) << endl;
+   // set p_max limit
+   for (int x = 0; x < prime_numbers_count; ++x) {
+      if (prime_numbers[x] > p_max) {
+         prime_numbers_count = x + 1;
+         break;
+      }
+   }
 
-   /**
-    * @brief Generate potencial M
-    * M > m_min
-    * M < m_max
-    * M = (prime_numbers[0] * prime_numbers[1] * ... * prime_numbers[F])^2,
-    * where prime_numbers[F]^2 < m_max
-    */
    for (int start = 0; start < prime_numbers_count; ++start) {
       if (prime_numbers[start] * prime_numbers[start] > m_max)
          break;
-      m_combination(start, prime_numbers[start], m_max, sequence);
+      m_combination(start, prime_numbers[start], m_max, m_min, sequence);
    }
-
-   /**
-    * @brief Generate potencial A
-    * A < M
-    * A > min_p
-    * A = (prime_number[0] * primer_number[1] * ... * prime_number[F])
-    */
 
    return 0;
 }
